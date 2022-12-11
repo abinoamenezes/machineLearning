f319bf9bd79b3d69cf86f4cdbc2e75d10a296355
Abinoã Menezes <abinoamenezes89@gmail.com>
2022-12-11 09:50:08 -0300

    base

--- pre-processamento/firstAnalise.py
+++ pre-processamento/firstAnalise.py
@@ -4,3 +4,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
+
+
+base_credit=pd.read_csv('pre-processamento/credit_risk_dataset.csv')
+print(base_credit.head(10))
